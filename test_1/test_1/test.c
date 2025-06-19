5682e9b6191dfc954adf6c36508e3fa2ed477b84
1 <1>
2025-06-19 16:09:42 +0800

    测试GITHUB

---
+++ test_1/test_1/test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int a = 23;
+void test1()
+{
+	a = 20;
+
+}
+void test()
+{
+	 a = 11;
+
+}
+int main()
+{
+	//int num1 = 0;
+	//int num2 = 0;
+	//scanf("%d %d", &num1,&num2);
+	//int sum = num1 + num2;
+	printf("%d\n", a);
+
+	//printf("%zd\n",sizeof(char));
+	//printf("%zd\n", sizeof(short));
+	//printf("%zd\n", sizeof(int));
+	//printf("%zd\n", sizeof(long));
+	//printf("%zd\n", sizeof(long long));
+	//printf("%zd\n", sizeof(float));
+	//printf("%zd\n", sizeof(double));
+	return 0;
+
+}
+
+
+//int main() {
+// ,/
+//	printf("hehe\n");
+//
+//	printf("hehe\n");
+//
+//	printf("hehe\n");
+//	printf("hehe\n");
+//
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	return  0;
+//}
+
